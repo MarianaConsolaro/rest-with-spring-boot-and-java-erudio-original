e5d3255a6b73b7a9f4176b01333a9eaf61869d40
Mariana <mariana.perez.consolaro@gmail.com>
2024-02-05 16:47:23 -0300

    Seção 11 - Migration
    Adicionando suporte a Migrations com Flyway.

---
+++ src/main/resources/db/migration/V2__Pop.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO `person` VALUES (1,'São Paulo','Mariana','Fem','Consolaro'),(2,'Anchiano - Italy','Leonardo','Male','Da Vinci'),(4,'Porbandar - India','Indira','Female','Gandhi'),(5,'Porbandar - India','Mahatma','Male','Gandhi'),(7,'Kentuchy - USA','Muhamad','Male','Ali'),(9,'Mvezo - South Africa','Nelson','Male','Mandela'),(10,'Smijan - Croatia','Nikola','Male','Tesla');
+
